bd86782639a67de6322b559da4565b6550f179e6
Julio Daniel Reyes <nemesiscodex@gmail.com>
2015-09-04 06:21:08 +0000

    initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('workspaceApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
