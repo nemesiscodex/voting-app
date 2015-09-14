23011b4155e7e4bc435770df628da26e33cd2df7
Julio Reyes <nemesiscodex@gmail.com>
2015-09-14 10:03:50 -0400

    Facebook share + comment

--- client/app/polls/polls.controller.js
+++ client/app/polls/polls.controller.js
@@ -186,6 +186,14 @@ angular.module('workspaceApp')
 				$timeout(drawChart);
 			});
 
+				(function(d, s, id) {
+					var js, fjs = d.getElementsByTagName(s)[0];
+					if (d.getElementById(id)) return;
+					js = d.createElement(s); js.id = id;
+					js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.4&appId=682746471855920";
+					fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));
+
 		}
 
 	});
