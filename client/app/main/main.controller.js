a5d04ba331d4dcd4dea54719cc7c42cdb98b0e70
Julio Reyes <nemesiscodex@gmail.com>
2015-09-17 02:38:54 -0400

    "Discover or Create" replace "Signup or Login" when authenticated

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('workspaceApp')
-  .controller('MainCtrl', function ($scope, $http, $translate) {
+  .controller('MainCtrl', function ($scope, $http, $translate, Auth) {
+    $scope.isLoggedIn = Auth.isLoggedIn;
+
     $translate('MAIN.OR').then(function(or){
       angular.element('#main-style').html('.signup-login .ui.buttons .or:before{ content: "' + or + '" !important; }');
     })
